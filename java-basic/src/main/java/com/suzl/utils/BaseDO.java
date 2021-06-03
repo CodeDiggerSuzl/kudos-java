82703f490e9c5896e533ca97a725bf4b7daa2a0c
suzailong <joexsue@foxmail.com>
2021-06-03 13:03:18 +0800

    :bulb: add code
    add code

---
+++ java-basic/src/main/java/com/suzl/utils/BaseDO.java
@@ -0,0 +1,62 @@
+package com.suzl.utils;
+
+import java.util.Date;
+
+/**
+ * <p></p>
+ *
+ * @author suzailong
+ * @date 2021/5/29 6:04 下午
+ */
+public class BaseDO {
+
+    /**
+     * 主键id
+     */
+    private Long id;
+    /**
+     * 租户id
+     */
+    private Long tenantId;
+    /**
+     * 创建时间
+     */
+    private Date gmtCreate;
+    /**
+     * 修改时间
+     */
+    private Date gmtModified;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(Long tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+}
