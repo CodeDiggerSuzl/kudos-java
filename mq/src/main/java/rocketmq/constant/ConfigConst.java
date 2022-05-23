969526a5e609edad94bb8cd56a29be00f8727db4
suzailong <suzailong@didachuxing.com>
2022-05-23 16:42:24 +0800

    feat:rocketmq

--- mq/src/main/java/rocketmq/constant/ConfigConst.java
+++ mq/src/main/java/rocketmq/constant/ConfigConst.java
@@ -9,4 +9,7 @@ public class ConfigConst {
      * name server 地址
      */
     public static final String ROCKET_MQ_NAMESRV_ADDR = "127.0.0.1:9876";
+
+    public static final String TOPIC_NAME = "test-topic";
+
 }
